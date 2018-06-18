c580808bd2c740358733dc0d37a1532457e53f0b
ivancaminal <ivancaminal72@gmail.com>
2018-06-18 20:05:22 +0200

    improve parser

--- adapt/KITTY/kitty_to_png/main.cpp
+++ adapt/KITTY/kitty_to_png/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sstream>
@@ -8,38 +9,41 @@
 
 namespace bf = boost::filesystem;
 
+void parse_error()
+{
+    printf("Try `%s --help' for more information.\n", argv[0]);
+    exit(1);
+}
+
 int main(int argc, char **argv)
 {
-    if (argc == 1)
-    {
-        printf("Try `%s --help' for more information.\n", argv[0]);
-        exit(1);
-    }
+    if (argc == 1) parse_error();
 
     bf::path lidarPath, imagePath;
     float scaling, downsampling;
 
     /*****************************Parser*****************************/
     int c, longindex=0;
-    const char *short_opt = "s:d:l:i:h";
+    const char *short_opt = "l:s:i::h";
     static struct option long_opt[] = {
-        {"scaling",         required_argument,      0,   's' },
-        {"downsampling",    required_argument,      0,   'd' },
-        {"lidar",           required_argument,      0,   'l' },
-        {"image",           required_argument,      0,   'i' },
-        {"help",            no_argument,            0,   'h' },
-        {0,                 0,                      0,   0   }
+        {"scaling",         required_argument,      0,   255+1  },
+        {"downsampling",    required_argument,      0,   255+2  },
+        {"lidar",           required_argument,      0,   'l'    },
+        {"save",            required_argument,      0,   's'    },
+        {"image",           optional_argument,      0,   'i'    },
+        {"help",            no_argument,            0,   'h'    },
+        {0,                 0,                      0,   0      }
     };
 
     while ((c = getopt_long(argc, argv, short_opt, long_opt, &longindex)) != -1)
     {
         switch (c)
         {
-        case 's':
+        case 255+1:
             scaling = atof(optarg);
             printf("scaling '%f'\n", scaling);
             break;
-        case 'd':
+        case 255+2:
             downsampling = atof(optarg);
             printf("downsampling '%f'\n", downsampling);
             break;
@@ -67,13 +71,15 @@ int main(int argc, char **argv)
         }
     }
 
-    if(is_empty(imagePath)) imagePath = lidarPath;
     if (optind < argc)
     {
         printf("non-option ARGV-elements: ");
         while (optind < argc)
             printf("%s \n", argv[optind++]);
+        parse_error();
     }
+
+    if(is_empty(imagePath)) imagePath = lidarPath;
 
     /*****************************Program*****************************/
 
